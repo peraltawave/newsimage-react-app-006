ffe2cfdb45950c6494cafb97604f0073e2ec5187
DMaloney10 <derek.maloney10@gmail.com>
2019-01-22 19:39:08 -0700

    updated saved and removed books info

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
-const bookRoutes = require("./books");
+
 const articlesRoutes = require("./articles");
 const savedsRoutes = require("./saveds");
 
 
-// Book routes
-router.use("/books", bookRoutes);
+// Article routes
+
 router.use("/articles", articlesRoutes);
 router.use("/saveds", savedsRoutes);
 
